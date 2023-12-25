f3b3213165917d4aed0565911d9070671449a58c
mrbillgates2001 <shahboznabiyev2001@gmail.com>
2023-12-26 03:45:16 +0500

    About section created

--- src/components/Main/About_us/About_us.jsx
+++ src/components/Main/About_us/About_us.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
-import "./About_us.scss"
+import React from "react";
+import Calendar from "../../../assets/images/Calendar.svg";
+import Mastercard from "../../../assets/images/Mastercard.svg";
+import Plesos from "../../../assets/images/Plesos.svg";
+import "./About_us.scss";
 
 const About_us = () => {
-  return (
-    <section className="about">
-      <div className="container">
-        <div className="about-content">
-          
-        </div>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="about">
+			<div className="container">
+				<div className="about-content">
+					<h1>About Us</h1>
+					<p className="about-paragraph">
+						Sagittis nibh scelerisque vitae eget vulputate sem elementum sed
+						neque nisi felis non ultrices massa id egestas quam velit pretium
+						nu.
+					</p>
+					<div className="about-cards">
+						<div className="a-card">
+							<img src={Calendar} alt="" />
+							<h3>1. Schedule online</h3>
+							<p>
+								Sagittis nibh scelerisque vitae egetolment vulputate sem
+								elementum sed n.
+							</p>
+						</div>
 
-export default About_us
+						<div className="a-card">
+							<img src={Plesos} alt="" />
+							<h3>2. Pay online easily</h3>
+							<p>
+								Vitae ut accumsan blandit ullamcorperolm suscipit dui gravida
+								amet at nunc.
+							</p>
+						</div>
+
+						<div className="a-card">
+							<img src={Mastercard} alt="" />
+							<h3>3. Get your house cleaned</h3>
+							<p>
+								Nunc maecenas sollicitudin metus tellus mattis sed porttitor
+								cursus eleifend.
+							</p>
+						</div>
+					</div>
+
+					<div className="about-btns">
+            <button className="btn-primary">Get a free quote</button>
+            <button className="btn-secondary">Explore services</button>
+          </div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About_us;
--- src/components/Main/Services/Services.jsx
+++ src/components/Main/Services/Services.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import House from "../../../assets/images/House.png"
+import Office from "../../../assets/images/Office.png"
+import Industries from "../../../assets/images/Industries.png"
 import "./Services.scss"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="service">
+      <div className="container">
+        <div className="service-content">
+          <div className="service-heading">
+            <h1>Our Services</h1>
+            <button className='btn-secondary'>Explore services</button>
+          </div>
+
+          <div className="service-cards">
+            <div className="s-card">
+              <img src={House} alt="" />
+              <h6>House cleaning</h6>
+              <p>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalaracc lacus vel facilisis volutpat est.</p>
+            </div>
+
+            <div className="s-card">
+              <img src={Office} alt="" />
+              <h6>House cleaning</h6>
+              <p>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalaracc lacus vel facilisis volutpat est.</p>
+            </div>
+
+            <div className="s-card">
+              <img src={Industries} alt="" />
+              <h6>House cleaning</h6>
+              <p>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalaracc lacus vel facilisis volutpat est.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
